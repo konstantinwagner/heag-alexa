e07760507b20ff0f8a4362e8ca8a3bc0513fc7aa
P. K. Wagner <git@alphath.space>
2019-01-21 02:30:38 +0100

    CF: Add user table

--- src/handler/handler.ts
+++ src/handler/handler.ts
@@ -3,8 +3,7 @@ import {NextDeparturesHandler} from './NextDepartures.handler';
 import {FavoriteStationHandler} from './FavoriteStation.handler';
 
 export const alexa = Ask.SkillBuilders.custom()
-    .addRequestHandlers(NextDeparturesHandler, FavoriteStationHandler)
-    /*.addRequestHandler(() => true, input => {
-        return input.responseBuilder.speak('hello').getResponse();
-    })*/
-  .lambda();
+    .addRequestHandlers(
+        NextDeparturesHandler,
+        FavoriteStationHandler)
+    .lambda();
